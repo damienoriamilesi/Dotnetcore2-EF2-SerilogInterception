74db8f60129d458e10ee4285b92f50212630c65f
MICROPOLE\doria <doria@cross-systems.com>
2018-08-14 14:51:01 +0200

    Add DataAnnotations and ModelBinding for the ID

--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -1,12 +1,24 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BethaniePieShop.Models
 {
     [Table("Feedback")]
     public class Feedback
     {
+        [BindNever]
         public int Id { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Your name is required")]
         public string Name { get; set; }
+
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Your email is required")]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]
+        // bool isEmail = Regex.IsMatch(emailString, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
         public string Email { get; set; }
         public string Message { get; set; }
         public bool ContactMe { get; set; }
